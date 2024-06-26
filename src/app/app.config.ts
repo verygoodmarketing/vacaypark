c11ee6f1e1d8904401ac35c958230b59e81086ae
Brad Bodine <bbodine1@gmail.com>
2024-06-25 20:26:35 -0500

    add firebase and firestore to app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,26 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environments';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({ ...environment.firebase })),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideFirestore(() => getFirestore()),
+  ],
 };
---
+++ src/environments/environments.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDsT7eAsZVGBwOBjy7agNDyQKdHKGo1crA',
+    authDomain: 'vacaypark.firebaseapp.com',
+    databaseURL: 'https://YOUR_PROJECT_ID.firebaseio.com',
+    projectId: 'vacaypark',
+    storageBucket: 'vacaypark.appspot.com',
+    messagingSenderId: '18705582723',
+    measurementId: 'G-Z8N1K0EXBN',
+    appId: '1:18705582723:web:3071a8417bf59f88f169b6',
+  },
+};
